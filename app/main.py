463646916586891f82ebf495e77c5f6fd71a44a8
Eldinosaur <82054019+Eldinosaur@users.noreply.github.com>
2025-04-15 05:07:18 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import Base, engine
 from app.routers import auth
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes reemplazar "*" por la IP o dominio exacto si deseas restringir
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(auth.router, prefix="/api", tags=["Auth"])
